85fa694fe99d7b1a0a3e4962f7c8fbfd9d167ae8
Th3RapidK1ller <stcikeri@bx.fallmerayer.it>
2021-04-15 20:16:42 +0200

    Final

--- Management/add_sale.php
+++ Management/add_sale.php
@@ -122,43 +122,9 @@ if (!empty($_POST['selected_product']) and !empty($_POST['selected_client']) and
     text-align: center;
     margin-top: 10px;
     margin-left: 28.5%;">
-    <table class="table table-bordered border-dark sortable">
-        <thead>
-        <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Client ID</th>
-            <th scope="col">Item Name</th>
-            <th scope="col">Price per Item</th>
-            <th scope="col">Quantity</th>
-            <th scope="col">Total Cost</th>
-        </tr>
-        </thead>
-        <tbody>
-        <?php
-        try {
-            $handler = new PDO('mysql:dbname=supermarket;host=localhost', 'root', '');
-        } catch (Exception $e) {
-            print "Error!: " . $e->getMessage() . "<br/>";
-            die();
-        }
-
-        $sql = $handler->prepare('select v_id, f_k_id, p_name,p_preis, v_menge, sum(p_preis * v_menge) as "Total Cost" from Kunde, Verkauft, Produkt where f_k_id = Kunde.k_id and f_p_id = Produkt.p_id group by v_id');
-        $sql->execute();
-        while ($result = $sql->fetch(PDO::FETCH_ASSOC)) {
-            echo "<tr>";
-            echo '<td>' . $result['v_id'] . '</td>';
-            echo '<td>' . $result['f_k_id'] . '</td>';
-            echo '<td>' . $result['p_name'] . '</td>';
-            echo '<td>' . $result['p_preis'] . '€</td>';
-            echo '<td>' . $result['v_menge'] . '</td>';
-            echo '<td>' . $result['Total Cost'] . '€</td>';
-            echo "</tr>";
-        }
-
-        ?>
-
-        </tbody>
-    </table>
+    <?php
+        include "report_stock.php";
+    ?>
 </div>
 <script>
     let mySele = document.getElementById("mySelection");
